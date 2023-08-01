533e9c9d57bfedb59a7e5a9f8947fba2e368aadd
ChileKasoka <chilekesha.kasoka@cs.unza.zm>
2023-08-01 09:41:54 +0300

    made header more responsive and nav hover effects

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,33 +7,68 @@ import { FaSearch } from 'react-icons/fa';
 
 function Header() {
   const [query, setQuery] = useState('');
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const toggleSearch = () => {
+    if (isSearchOpen) {
+      // If the search bar is open
+      if (query.trim() === '') {
+        // If the query is empty, close the search bar
+        setIsSearchOpen(false);
+      } else {
+        // If the query is not empty, submit the form
+        handleSubmit(); // Call the submit function directly
+      }
+    } else {
+      // If the search bar is closed, open it
+      setIsSearchOpen(true);
+    }
+  };
+  
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Search Query:', query);
+    setQuery('');
+  }
+
   return (
-    <header className='w-full h-40 border-b py-4 sm:block'>
-      <div className='w-4/5 mx-auto py-5 px-10 flex flex-shrink justify-between items-center pb-2'>
-        <div className='flex items-center mx-4'>
+    <header className='w-full h-fit border-b py-4 sm:block'>
+      <div className='w-4/5 mx-auto py-5 px-10 grid grid-cols-3 gap-2 items-center pb-2'>
+        <div className='flex items-center'>
           <Image src="/Workapp.png" alt="logo" width="120" height="120" />
         </div>
-        <div className="hidden sm:flex items-center border rounded-md shadow-sm">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 rounded-md"
-            value={query}
-            onChange={handleInputChange}
-          />
-          <button className="px-4 py-2 bg-black rounded-r-md">
-            <FaSearch
-              className="text-white h-6"
+        <div className='flex justify-end px-4'>
+        <form onSubmit={handleSubmit} className={`sm:flex items-center border rounded-md shadow-sm ${query.trim() ? 'block' : 'hidden'}`}>
+            <label htmlFor="searchInput" className="sr-only">
+              Search
+            </label>
+            <input
+              id="searchInput"
+              type="text"
+              placeholder="Search..."
+              className="px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 rounded-md"
+              value={query}
+              onChange={handleInputChange}
             />
-          </button>
+            <button type="submit" className="px-4 py-2 bg-black rounded-r-md">
+              <FaSearch className="text-white h-6" />
+            </button>
+        </form>
+
+          {/* <button type="submit" onClick={toggleSearch} className="px-4 py-2 bg-black rounded-r-md">
+              <FaSearch
+                className="text-white h-6"
+              />
+          </button> */}
         </div>
+
         {/* sign buttons should be hidden but anything above sm should display flex */}
-        <div className='hidden md:flex flex-wrap flex-shrink'>
+        <div className='hidden lg:flex flex-wrap flex-shrink'>
           <button className="bg-gray-500 text-white mr-5 px-4 py-2 rounded">
             Sign In
           </button>
@@ -42,7 +77,7 @@ function Header() {
           </button>
         </div>
       </div>
-      <div className='bg-blue-gray-700 w-full mx-auto'>
+      <div className='w-4/5 bg-blue-gray-700 mx-auto'>
         <Navbar />
       </div>
     </header>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,43 +11,35 @@ function Navbar() {
         setNav(!nav)
     }
 
+    const navigationLinks = [
+        { id: 1, text: 'Home', href: '/' },
+        { id: 2, text: 'Business', href: '/business' },
+        { id: 3, text: 'Pricing', href: '/pricing' },
+        { id: 4, text: 'Contact Us', href: '/contact' },
+        { id: 5, text: 'jobs', href: '/jobs' },
+        { id: 6, text: 'Workers', href: '/workers' },
+        { id: 7, text: 'Find People', href: '/find-people' },
+        { id: 8, text: 'Events', href: '/events' },
+        { id: 9, text: 'Our Services', href: '/services' }
+        // Add more navigation links as needed
+      ];
+
   return (
-    <div className='flex justify-center py-2 bg-blue-gray-600 border-y border-slate-500'>
-        <div>
-            <ul className='flex flex-wrap p-2 space-x-4 hidden md:flex shrink items-center'>
-                <Link href="/">
+    <div className='flex justify-center p-8 bg-blue-gray-600 border-y md:border-slate-500'>
+        <div className='container '>
+            <ul className='hidden divide-y max-w-full h-fit p-5 md:flex'>
+                {/* <Link href="/">
                     <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>home</li>
-                </Link>
-                <Link href="/business">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>Business</li>
-                </Link>
-                <Link href="/workers">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>Workers</li>
-                </Link>
-                <Link href="/jobs">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>Jobs</li>
-                </Link>
-                <Link href="/auto">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>Auto</li>
-                </Link>
-                <Link href="/community">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>Community</li>
-                </Link>
-                <Link href="/events">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>Events</li>
-                </Link>
-                <Link href="/find-people">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>Find People</li>
-                </Link>
-                <Link href="/About">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>About</li>
-                </Link>
-                <Link href="/services">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>Our Services</li>
-                </Link>
-                <Link href="/contact">
-                    <li className='hover:text-amber-600 ml-4 hover:border-b text-sm lg:text-xl'>Contact Us</li>
-                </Link>
+                </Link> */}
+                <nav className="hidden text-sm m-auto md:flex flex-wrap p-2 lg:text-xl space-x-4 items-center">
+                    {navigationLinks.map((link) => (
+                        <Link key={link.id} href={link.href}>
+                            <div className="group flex-auto relative rounded overflow-hidden hover:shadow-md transition-shadow">
+                                <li className="block text-gray-600 hover:text-white hover:bg-black p-2 rounded hover:rounded-lg">{link.text}</li>
+                            </div>
+                        </Link>
+                    ))}
+                </nav>
             </ul>
         </div>
 
@@ -56,7 +48,7 @@ function Navbar() {
             {nav ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30}/>}
         </div>
 
-        <div className={nav ? 'md:hidden absolute z-10 left-0 right-0 top-24 h-80 bottom-0 w-full bg-gray-200 flex justify-center items-center duration-300' : 'md:hidden absolute z-10 top-[-100%] right-0 top-24 h-80 bottom-0 w-full bg-gray-200 flex justify-center items-center duration-300'}>
+        <div className={nav ? 'md:hidden absolute z-10 left-0 right-0 top-24 h-80 bottom-0 w-full bg-gray-200 flex justify-center items-center duration-300' : 'md:hidden absolute z-10 top-[-100%] right-0 h-80 bottom-0 w-full bg-gray-200 flex justify-center items-center duration-300'}>
             <ul className='bg-slate-600 text-white p-10 absolute top-0 right-0 w-full'>
                 <Link href="/">
                     <li className='block py-4 text-center hover:text-amber-600 text-xl'>Home</li>
